e4ab837628e814aae918ce01cb9b462e23ac728b
belmeopmenieuwesim <belmeopmenieuwesim@protonmail.com>
2023-07-18 15:23:28 +0200

    add dependency injection extension

---
+++ src/DependencyInjection/DoctrineCockroachDBExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineCockroachDB\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class DoctrineCockroachDBExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $locator = new FileLocator(dirname(__DIR__) . '/../../Resources/config');
+        $loader = new YamlFileLoader($container, $locator);
+
+        $loader->load('services.yaml');
+    }
+}
